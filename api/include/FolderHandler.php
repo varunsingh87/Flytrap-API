1f1760faf819930045833259dd787fafe67d866f
Varun <31084854+Borumer@users.noreply.github.com>
2021-03-17 21:03:01 -0400

    Implement folder endpoint GET * Implement FolderHandler#getFolder() * Implement folder.php with - headers - SimpleRest calls - FolderHandler object instantiation - GET method response

--- api/include/FolderHandler.php
+++ api/include/FolderHandler.php
@@ -2,17 +2,48 @@
 
 namespace VarunS\Flytrap\DBHandlers;
 
+use VarunS\Flytrap\EndpointResponse;
+
 class FolderHandler {
     protected UserChecker $dbChecker;
     protected $folderId;
 
-    public function __construct($userApiKey, $folderId)
+    public function __construct($userApiKey)
     {
         $this->dbChecker = new UserChecker($userApiKey);
+    }
+
+    public function setFolderId($folderId) {
         $this->folderId = $folderId;
     }
 
-    
+    public function getFolder() {
+        if ($this->folderId == NULL) {
+            return [
+                "statusCode" => 500,
+                "error" => [
+                    "message" => "A server error occurred",
+                    "developer" => "The folder id was not specified by the API endpoint"
+                ]
+            ];
+        }
+
+        $audioFiles = $this->dbChecker->executeQuery("SELECT * FROM folders WHERE user_id = " . $this->dbChecker->userId . " AND id = " . $this->folderId);
+
+        if (!((bool) $audioFiles)) {
+            return [
+                "statusCode" => 404,
+                "error" => [
+                    "message" => "You do not have access to that folder or it does not exist"
+                ]
+            ];
+        }
+
+        $audioFiles = mysqli_fetch_all($audioFiles, MYSQLI_ASSOC);
+
+        $response = new EndpointResponse($audioFiles);
+        return $response;
+    }
 }
 
 ?>
