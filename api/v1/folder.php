530ee3c6281d0df2bad01f9043fd82ad96045378
Varun <31084854+Borumer@users.noreply.github.com>
2021-03-17 21:03:01 -0400

    Implement folder endpoint GET * Implement FolderHandler#getFolder() * Implement folder.php with - headers - SimpleRest calls - FolderHandler object instantiation - GET method response

--- api/v1/folder.php
+++ api/v1/folder.php
@@ -1,5 +1,27 @@
-<?php 
+<?php
 
+use VarunS\Flytrap\DBHandlers\FolderHandler;
+use VarunS\PHPSleep\SimpleRest;
 
+header("Access-Control-Allow-Headers: Authorization,authorization");
+header("Access-Control-Expose-Headers: Authorization,authorization");
+header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"] . "");
+header("Access-Control-Allow-Credentials: true");
+
+$headers = apache_request_headers();
+
+SimpleRest::handleHeaderValidation($headers, "authorization");
+SimpleRest::handleRequestMethodValidation("GET", "OPTIONS");
+
+$folderHandler = new FolderHandler(SimpleRest::parseAuthorizationHeader($headers["authorization"]));
+
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET':
+        $folderHandler->setFolderId($_GET['folder_id']);
+        $response = $folderHandler->getFolder();
+        SimpleRest::setHttpHeaders($response["statusCode"]);
+        echo json_encode($response);
+        break;
+}
 
 ?>
