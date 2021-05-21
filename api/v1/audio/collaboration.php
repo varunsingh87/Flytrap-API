5235ad6be244c51116f16b6b1295db8874753f7c
Varun <31084854+Borumer@users.noreply.github.com>
2021-05-20 22:15:19 -0400

    Implement POST audio/collaboration

---
+++ api/v1/audio/collaboration.php
@@ -0,0 +1,68 @@
+<?php
+
+require __DIR__ . "/../../vendor/autoload.php";
+
+use Flytrap\DBHandlers\AudioHandler;
+use Flytrap\DBHandlers\FolderHandler;
+use VarunS\PHPSleep\SimpleRest;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__, "dbconfig.env");
+$dotenv->safeLoad();
+
+header("Access-Control-Allow-Headers: Authorization,authorization");
+header("Access-Control-Expose-Headers: Authorization,authorization");
+header("Access-Control-Allow-Credentials: true");
+
+if (isset($_SERVER["HTTP_ORIGIN"])) {
+    header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
+} else {
+    $_SERVER["HTTP_ORIGIN"] = "localhost";
+}
+
+SimpleRest::handleRequestMethodValidation("GET", "POST", "PUT", "DELETE", "OPTIONS");
+
+if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") exit();
+
+$headers = apache_request_headers();
+
+SimpleRest::handleHeaderValidation($headers, "authorization");
+
+$dbHandler = new AudioHandler(SimpleRest::parseAuthorizationHeader($headers["authorization"]));
+
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET':
+        // $dbHandler->setAudioId($_GET['audio_id']);
+        // $response = $dbHandler->getAudio();
+        // SimpleRest::setHttpHeaders($response["statusCode"]);
+        
+        // echo json_encode($response);
+        break;
+    case 'POST':
+        SimpleRest::handlePostParameterValidation("recipient_email", "audio_id");
+        $dbHandler->setAudioId($_POST["audio_id"]);
+        $response = $dbHandler->shareAudio($_POST["recipient_email"]);
+        
+        SimpleRest::setHttpHeaders($response["statusCode"]);
+        echo json_encode($response);
+        break;
+    case 'PUT':
+        // $put = [];
+        // parse_str(file_get_contents("php://input"), $put);
+        // $dbHandler->setAudioId($put['audio_id']);
+        // $response = $dbHandler->renameAudio($put['new_name']);
+        // SimpleRest::setHttpHeaders($response["statusCode"]);
+        // echo json_encode($response);
+        break;
+    case 'DELETE':
+        // parse_str(file_get_contents("php://input"), $GLOBALS["_{DELETE}"]);
+        // $dbHandler->setAudioId($GLOBALS["_{DELETE}"]['audio_id']);
+
+        // SimpleRest::handleDeleteParameterValidation("audio_id");
+
+        // $response = $dbHandler->deleteAudio();
+        // SimpleRest::setHttpHeaders($response["statusCode"]);
+        // echo json_encode($response);
+        break;
+}
+
+?>
